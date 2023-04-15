c218087167180d08053b18e6e0914fe517135896
alexmarab <130222615+alexmarab@users.noreply.github.com>
2023-04-15 19:10:35 +0100

    feat(*): Create products state in HomeScreen
      Change data by products state

--- src/screens/HomeScreen.jsx
+++ src/screens/HomeScreen.jsx
@@ -1,12 +1,16 @@
+import { useEffect, useState } from 'react';
 import { Link } from 'react-router-dom';
-import data from '../data';
+
+// import data from '../data';
 
 function HomeScreen() {
+  const [products, setProducts] = useState([]);
+
   return (
     <div>
       <h1>Featured Products</h1>
       <div className="products">
-        {data.products.map((product) => (
+        {products.map((product) => (
           <div className="product" key={product.slug}>
             <Link to={`/product/${product.slug}`}>
               <img src={product.image} alt={product.name} />
