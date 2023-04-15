47d2990ebc789d7038b649179fc9a1dd1ac73463
alexmarab <130222615+alexmarab@users.noreply.github.com>
2023-04-15 13:27:44 +0100

    feat(*): create header and main div in App component
    Add header and main div in App component

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,14 @@
-import "./App.css";
+import './App.css';
 
 function App() {
-  return <div className="App">Inicio</div>;
+  return (
+    <div className="App">
+      <header className="header">
+        <a href="/">amazona</a>
+      </header>
+      <main>list products</main>
+    </div>
+  );
 }
 
 export default App;
