2bb131ec0ec1e21f8662173e1d73ed78a70f1ffd
alexmarab <130222615+alexmarab@users.noreply.github.com>
2023-04-16 10:47:31 +0100

    feat(*): Create Rating component
      - Import font-awesome in index.html
      - Change star color with .rating span css class
      - Change Botton primary-color in css

--- src/components/Product.jsx
+++ src/components/Product.jsx
@@ -2,6 +2,7 @@
 import { Link } from 'react-router-dom';
 import Card from 'react-bootstrap/Card';
 import Button from 'react-bootstrap/Button';
+import Rating from './Rating';
 
 const Product = (props) => {
   const { product } = props;
@@ -14,6 +15,7 @@ const Product = (props) => {
         <Link to={`/product/${product.slug}`}>
           <Card.Title>{product.name}</Card.Title>
         </Link>
+        <Rating rating={product.rating} numReviews={product.numReviews} />
         <Card.Text>{product.price} €</Card.Text>
         <Button>Add to cart</Button>
       </Card.Body>
