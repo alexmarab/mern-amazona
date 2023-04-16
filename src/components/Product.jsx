917b47f1e6f0c749e51d5ebedb36fb28df3a3e00
alexmarab <130222615+alexmarab@users.noreply.github.com>
2023-04-16 10:30:02 +0100

    feat(*): Create Product component
      - Create folder components
      - Create Product Component using Card component
      - Cut and paste code from products.map
      - Replace div by Card

---
+++ src/components/Product.jsx
@@ -0,0 +1,24 @@
+/* eslint-disable react/prop-types */
+import { Link } from 'react-router-dom';
+import Card from 'react-bootstrap/Card';
+import Button from 'react-bootstrap/Button';
+
+const Product = (props) => {
+  const { product } = props;
+  return (
+    <Card key={product.slug}>
+      <Link to={`/product/${product.slug}`}>
+        <img src={product.image} className="card-img-top" alt={product.name} />
+      </Link>
+      <Card.Body>
+        <Link to={`/product/${product.slug}`}>
+          <Card.Title>{product.name}</Card.Title>
+        </Link>
+        <Card.Text>{product.price} €</Card.Text>
+        <Button>Add to cart</Button>
+      </Card.Body>
+    </Card>
+  );
+};
+
+export default Product;
