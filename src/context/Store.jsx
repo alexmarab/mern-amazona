0fa15af0088cfa59709f4a2821a72ba9afee5044
alexmarab <130222615+alexmarab@users.noreply.github.com>
2023-04-27 16:51:41 +0100

    feat(*): Handler Buttons Cart Screen
      - Click handler for inc/dec item (UpdateCartHandler func)
      - Click handler for remove item (removeItemHandler func)
      - Add action CART_REMOVE_ITEM into action reducer (Store)

--- src/context/Store.jsx
+++ src/context/Store.jsx
@@ -26,6 +26,15 @@ const reducer = (state, action) => {
 
       return { ...state, cart: { ...state.cart, cartItems } };
     }
+
+    case 'CART_REMOVE_ITEM': {
+      const cartItems = state.cart.cartItems.filter(
+        (item) => item._id !== action.payload._id
+      );
+
+      return { ...state, cart: { ...state.cart, cartItems } };
+    }
+
     default:
       return state;
   }
