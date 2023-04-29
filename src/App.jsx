b45e633f43bd03d40ac1d18f8cb32d3424698b80
alexmarab <130222615+alexmarab@users.noreply.github.com>
2023-04-29 13:09:54 +0100

    feat(*): Create ShippingAddressScreen
      - Create Shipping Address form
      - Create new action SAVE_SHIPPING_ADDRESS
      - Save shipping address into local storage
      - Avoid access to shipping screeen if not logged
      - Modify USER_SIGNOUT action to initialize cart object

--- src/App.jsx
+++ src/App.jsx
@@ -15,6 +15,7 @@ import { useContext } from 'react';
 import { Store } from './context/Store';
 import CartScreen from './screens/CartScreen';
 import SigninScreen from './screens/SigninScreen';
+import ShippingAddressScreen from './screens/ShippingAddressScreen';
 
 function App() {
   const { state, dispatch: ctxDispatch } = useContext(Store);
@@ -77,6 +78,7 @@ function App() {
               <Route path="/product/:slug" element={<ProductScreen />} />
               <Route path="/cart" element={<CartScreen />} />
               <Route path="/signin" element={<SigninScreen />} />
+              <Route path="/shipping" element={<ShippingAddressScreen />} />
               <Route path="/" element={<HomeScreen />} />
             </Routes>
           </Container>
